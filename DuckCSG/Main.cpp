0fb67a3e0192360537e5723cefbac53809d566b0
Daniels <voiceofwise1@gmail.com>
2021-10-12 16:03:05 +0300

    First commit

---
+++ DuckCSG/Main.cpp
@@ -0,0 +1,7 @@
+#include "DuckCSGpch.h"
+
+int main(int argc, char** argv)
+{
+    std::cout << "DuckCSG" << std::endl;
+    return 0;
+}
---
+++ premake5.lua
@@ -0,0 +1,103 @@
+-- Duckie Map tools premake5 script
+
+workspace "DuckieMapTools"
+    architecture "x64"
+
+    configurations
+    {
+        "Debug",
+        "Release"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+-- Include directories
+includeDir = {}
+includeDir["spdlog"] = "subprojects/spdlog/include"
+
+-- spdlog project premake
+project "spdlog"
+    location "subprojects/spdlog"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "On"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("temp/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.location}/**.h"
+    }
+
+    includedirs
+    {
+        "%{includeDir.spdlog}"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
+
+-- DuckCSG project
+project "DuckCSG"
+    location "DuckCSG"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "on"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("temp/" .. outputdir .. "/%{prj.name}")
+
+    pchheader "DuckCSGpch.h"
+    pchsource "DuckCSG/DuckCSGpch.cpp"
+
+    files
+    {
+        "%{prj.location}/**.h",
+        "%{prj.location}/**.cpp"
+    }
+
+    defines
+    {
+        "_CRT_SECURE_NO_WARNINGS",
+        "PREMAKE",
+    }
+
+    includedirs
+    {
+        "%{prj.location}",
+        "%{includeDir.spdlog}"
+    }
+
+    filter "system:linux"
+        links
+        {
+            "pthread",
+            "fmt"
+        }
+
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        defines
+        {
+            "DUCKIEMAP_DEBUG"
+        }
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        defines "DUCKIEMAP_RELEASE"
+        runtime "Release"
+        optimize "on"
