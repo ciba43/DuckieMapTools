559beceabe8379d95258adc0dd5261105a922441
Daniels <voiceofwise1@gmail.com>
2021-10-17 15:28:21 +0300

    Run tests on startup if RUN_TESTS_ON_STARTUP is defined

--- DuckCSG/Main.cpp
+++ DuckCSG/Main.cpp
@@ -4,13 +4,28 @@
 
 #include "MapParser.h"
 
+#ifdef RUN_TESTS_ON_STARTUP
+#include "Tests/Tests.h"
+#endif
 int main(int argc, char** argv)
 {
     DuckCSG::Log::init();
 
     DC_CORE_INFO("DuckCSG map compiler for Duckie game engine");
 
-    // Parse a map
+#ifdef RUN_TESTS_ON_STARTUP
+    // Run tests on startup
+    DC_CORE_TRACE("Running tests ...");
+    MapTools::Test01ParseOneBrush test01;
+    auto result = test01.run();
+
+    if (result == MapTools::TestResult::Pass)
+        DC_CORE_INFO("01: PASS");
+    else
+        DC_CORE_ERROR("01: FAIL : {0}", test01.failDescription());
+#endif
+
+    // Regular parsing
     MapTools::MapParser mapParser("test01_fail.map");
 
     auto parserResult = mapParser.parse();
