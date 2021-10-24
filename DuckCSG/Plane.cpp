e4ec45d76da9a859ee4119c9ae5034da9c231dac
Daniels <voiceofwise1@gmail.com>
2021-10-24 12:10:51 +0300

    Fix unimplemented plane function messages

--- DuckCSG/Plane.cpp
+++ DuckCSG/Plane.cpp
@@ -9,36 +9,36 @@ namespace MapTools {
 
     void Plane::setPoint1(PlanePoint point)
     {
-        DC_CORE_ERROR("Unimplemented Brush::SetPoint1");
+        DC_CORE_ERROR("Unimplemented Plane::SetPoint1");
     }
 
     void Plane::setPoint2(PlanePoint point)
     {
-        DC_CORE_ERROR("Unimplemented Brush::setPoint2");
+        DC_CORE_ERROR("Unimplemented Plane::setPoint2");
     }
 
     void Plane::setPoint3(PlanePoint point)
     {
-        DC_CORE_ERROR("Unimplemented Brush::setPoint3");
+        DC_CORE_ERROR("Unimplemented Plane::setPoint3");
     }
 
     void Plane::setTextureString(std::string texture)
     {
-        DC_CORE_ERROR("Unimplemented Brush::setTextureString");
+        DC_CORE_ERROR("Unimplemented Plane::setTextureString");
     }
 
     void Plane::setUV(UV uv)
     {
-        DC_CORE_ERROR("Unimplemented Brush::setUV");
+        DC_CORE_ERROR("Unimplemented Plane::setUV");
     }
 
     void Plane::setScaleU(float scaleU)
     {
-        DC_CORE_ERROR("Unimplemented Brush::setScaleU");
+        DC_CORE_ERROR("Unimplemented Plane::setScaleU");
     }
 
     void Plane::setScaleV(float scaleV)
     {
-        DC_CORE_ERROR("Unimplemented Brush::setScaleV");
+        DC_CORE_ERROR("Unimplemented Plane::setScaleV");
     }
 }
