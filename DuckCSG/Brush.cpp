08c05a42eae7ce0914e1993b3c5d10b05a60afbc
Daniels <voiceofwise1@gmail.com>
2021-11-04 09:36:38 +0200

    Implement polygon generation at the intersections of planes

--- DuckCSG/Brush.cpp
+++ DuckCSG/Brush.cpp
@@ -2,6 +2,9 @@
 #include "Brush.h"
 
 #include "Plane.h"
+#include "Polygon.h"
+
+#include <glm/geometric.hpp>
 
 namespace MapTools {
 
@@ -12,6 +15,53 @@ namespace MapTools {
     void Brush::pushPlane(Ref<Plane> plane)
     {
         m_planes.push_back(plane);
+    }
+
+    void Brush::createPolygons()
+    {
+        for (size_t i = 0; i < m_planes.size() - 1; i++)
+        {
+            for (size_t j = 0; j < m_planes.size() - 1; j++)
+            {
+                for (size_t k = 0; k < m_planes.size() - 1; k++)
+                {
+                    if (i != j != k)
+                    {
+                        // Create the polygon if it doesn't exist
+                        if (i + 1 > m_polygons.size())
+                            m_polygons.push_back(createRef<Polygon>());
+                        getIntersection(i, j, k);
+                        m_polygons.at(i)->addVertex();
+                    }
+                }
+            }
+        }
+    }
+
+    std::pair<bool, glm::vec3> Brush::getIntersection(size_t plane1, size_t plane2, size_t plane3)
+    {
+        if (plane1 > m_planes.size() || plane2 > m_planes.size() || plane3 > m_planes.size())
+        {
+            DC_CORE_CRITICAL("Plane id out of bounds {0} > {1}", plane1, m_planes.size());
+            return { false, glm::vec3() };
+        }
+
+        auto firstPlane = m_planes.at(plane1);
+        auto secondPlane = m_planes.at(plane2);
+        auto thirdPlane = m_planes.at(plane3);
+
+        double denominator = glm::dot(firstPlane->normal(), glm::cross(secondPlane->normal(),thirdPlane->normal()));
+
+        // Is there no intersection (planes are parallel)?
+        if (denominator == 0)
+        {
+            return { false, glm::vec3() };
+        }
+
+        glm::vec3 point;
+        point = -firstPlane->distance() * glm::cross(secondPlane->normal(), thirdPlane->normal());
+
+        return std::pair<bool, glm::vec3>();
     }
 
     bool Brush::isValid()
--- DuckCSG/Brush.h
+++ DuckCSG/Brush.h
@@ -1,19 +1,28 @@
 #pragma once
 
+#include <glm/vec3.hpp>
 #include <string>
+#include <utility>
 #include <vector>
 
 namespace MapTools {
 
     class Plane;
+    class Polygon;
 
     class Brush
     {
     public:
         Brush();
         void pushPlane(Ref<Plane> plane);
+
+        void createPolygons();
+
+        std::pair<bool, glm::vec3> getIntersection(size_t plane1, size_t plane2, size_t plane3);
+
         bool isValid(); // A brush needs to be at least 4 planes to be considered valid (among other requirements)
     private:
         std::vector<Ref<Plane>> m_planes;
+        std::vector<Ref<Polygon>> m_polygons;
     };
 }
